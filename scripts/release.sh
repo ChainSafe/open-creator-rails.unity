128f7ffc6b296a31f864504a61802974d43dc402
rob1997 <robelgetnetrg@gmail.com>
2026-05-28 17:32:51 +0300

    release script

---
+++ scripts/release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+version=$1
+notes=$2
+
+if [ "$(git branch --show-current)" != "main" ]; then
+  git checkout main
+fi
+
+git pull
+
+./scripts/updateSubmodules.sh $version
+
+./scripts/duplicateSamples.sh
+
+git add io.chainsafe.open-creator-rails/
+
+echo "Please make sure ./SampleProject compiles and all tests pass, run ./scripts/localSetup.sh to run dependencies locally. "
+
+read -n 1 -p "Continue? (y/n): " response
+
+if [ $response != "Y" ] && [ $response != "y" ]; then
+  exit 1;
+fi
+
+git commit -m "Release $version" --allow-empty
+git push origin main
+
+gh release create "$version" \
+  --title "$version" \
+  --notes "$notes" \
+  deployments/*
+
+git tag "v$version" "$version"
+git push origin "v$version"
--- scripts/updateSubmodules.sh
+++ scripts/updateSubmodules.sh
@@ -3,6 +3,10 @@
 release_tag="$1"
 
 RELEASE_TAG="$release_tag" git submodule foreach '
+  git add *
+  git stash
+  git reset --hard
+  
   git fetch origin --tags
 
   default_ref=$(git symbolic-ref --short refs/remotes/origin/HEAD 2>/dev/null)
@@ -10,6 +14,8 @@ RELEASE_TAG="$release_tag" git submodule foreach '
 
   if [ -n "$RELEASE_TAG" ] && git rev-parse -q --verify "refs/tags/$RELEASE_TAG" >/dev/null; then
     git checkout "tags/$RELEASE_TAG"
+  elif [ -n "v$RELEASE_TAG" ] && git rev-parse -q --verify "refs/tags/v$RELEASE_TAG" >/dev/null; then
+    git checkout "tags/v$RELEASE_TAG"
   else
     git checkout "$default_branch"
     git pull origin "$default_branch"
