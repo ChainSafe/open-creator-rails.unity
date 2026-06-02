e1cb5d3a4fd3f4f27fb17ca93648d686baf74deb
rob1997 <robelgetnetrg@gmail.com>
2026-06-02 16:49:55 +0300

    ChainId filter on Indexer

--- io.chainsafe.open-creator-rails/Runtime/PonderIndexerProvider.cs
+++ io.chainsafe.open-creator-rails/Runtime/PonderIndexerProvider.cs
@@ -62,6 +62,7 @@ namespace Io.ChainSafe.OpenCreatorRails
 {{
     assets(
         where: {{
+            chainId: {OpenCreatorRailsService.Instance.WalletProvider.ChainId}
             assetId: ""{assetIdHash}""
             registryAddress: ""{registryAddress}""
         }}
